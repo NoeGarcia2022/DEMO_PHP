12903ee6207be1da51ed86d8e53ab62a9cbf1436
NoeGarcia2022 <noegarcia4789@gmail.com>
2022-05-05 17:02:18 -0600

    Creacion del directorio ITCA y ficheros PHP

---
+++ ITCA/gamez.php
@@ -0,0 +1,7 @@
+<?php
+
+	echo "Saludos a todos los estudiantes de sistemas SIS21B"
+
+	echo "Este es el contenido del archivo php"
+
+?>
---
+++ ITCA/manuel.php
@@ -0,0 +1,8 @@
+
+<?php
+
+	echo "Saludos a todos los estudiantes de sistemas SIS21B"
+
+	echo "Este es el contenido del archivo php"
+
+?>
